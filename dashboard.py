185eb5d8ea06b4a68092744014c096a8aec9a8b7
chiranjeetborah <70507399+chiranjeetborah@users.noreply.github.com>
2022-03-08 12:36:45 +0530

    commit

--- dashboard.py
+++ dashboard.py
@@ -5,52 +5,38 @@ import numpy as np
 import csv
 # How many cities are there?
 # How many restaurants have Table option
-# How many has online delivery
+# How many has the option of online delivery via their own app
 # Scatter chart of Aggregate rating
 # Bar chart of Text Rating
 # Exit
+df= pd.read_csv('zomato.csv',  encoding='iso-8859-1', nrows=1000, on_bad_lines = 'warn')
+groups=df.groupby(by='City')
+print(len(groups.groups.keys()))
 
-with open('zomato.csv', 'r', errors='ignore') as file:
-    reader = csv.reader(file)
-    row= []
-    city= []
-    resto= []
-    online_del= []
-    aggregrate_rating= []
-    for row in reader:
-        #city.append(row.city)
-        city.append(row[0])
-        resto.append(row[1])
-        online_del.append(row[2])
-        aggregrate_rating.append(row[3])
-print(resto)
-# START OF THE QUSTIONS
-# How many cities are there?
-def unique_c(city):
-    myset = set(city)
-    distinct_city_list=list(myset)
-    print("Number of Distinct cities are :", len(distinct_city_list))
 
-# How many restaurants have Table option
-def table_option(resto):
-    print("THE NUMBER OF RESTAURANTS WITH TABLE OPTION AVAILABLE ARE :", resto.count('Yes'))
+val=df[df['Has Table booking']=='Yes']
+print(val['City'].count())
 
-# How many has online delivery
 
-def online_dell(online_dell):
-    print("THE NUMBER OF RESTAURANTS WITH ONLINE DELIVERY ARE :", online_del.count('Yes'))
-print(online_del)
+val_online=df[df['Has Online delivery']=='Yes']
+print(val_online['City'].count())
 
-print("#####")
-print(unique_c(city))
-print("#####")
-print(table_option(resto))
-print("#####")
-print(online_dell(online_dell))
 
-# Scatter chart of Aggregate rating
-y=[]
-x = np.random.normal(min(y), max(y), len(y))
-        plt.scatter(x, y)
-        plt.savefig("scatter_sentiment_analysis.pdf")
-        plt.show()
+ratings=list(df['Aggregate rating'])
+x = np.random.normal(min(ratings), max(ratings), len(ratings))
+plt.scatter(x, ratings)
+plt.savefig("scatter_sentiment_analysis.pdf")
+plt.show()
+
+
+count_ratings=list(df['Rating text'])
+Excellent=(count_ratings.count('Excellent'))
+Very_good=(count_ratings.count('Very Good'))
+Average=(count_ratings.count('Average'))
+Good=(count_ratings.count('Good'))
+Poor=(count_ratings.count('Poor'))
+x=['Poor','Average', 'Good', 'Very Good',  'Excellent']
+y=[Poor,  Average,Good, Very_good, Excellent]
+plt.bar(x,y)
+plt.savefig("bar_sentiment_analysis.pdf")
+plt.show()
